e7e66e260e8c9a30fb5f3fce835c866e84087af9
Andres Montoya Vargas <andresmontoyavargas.av@gmail.com>
2025-10-28 20:25:52 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Comunidad_Conectada.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
