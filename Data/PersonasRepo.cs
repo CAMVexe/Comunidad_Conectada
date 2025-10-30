a93f117c3ec80bb408bf0523d4f93f43b8d323c8
Andres Montoya Vargas <andresmontoyavargas.av@gmail.com>
2025-10-30 16:42:38 -0600

    Creación de la clase con la lista (db) y métodos (show, add)

--- Data/PersonasRepo.cs
+++ Data/PersonasRepo.cs
@@ -4,5 +4,16 @@ namespace Comunidad_Conectada.Data
 {
     public class PersonasRepo
     {
+        private static readonly List<Persona> _personas = new();
+
+        private static int _nextId = 0;
+
+        public static IReadOnlyList<Persona> GetAll() => _personas.AsReadOnly();
+
+        public static void Agregar(Persona p)
+        {
+            p.ID = _nextId++;
+            _personas.Add(p);
+        }
     }
 }
