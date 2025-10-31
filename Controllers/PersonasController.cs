729a332a7deac732553c044e60a7aa88f841d416
Andres Montoya Vargas <andresmontoyavargas.av@gmail.com>
2025-10-31 15:16:41 -0600

    Contenido de la vista Index en Personas

---
+++ Controllers/PersonasController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Comunidad_Conectada.Models;
+using Comunidad_Conectada.Data;
+
+namespace Comunidad_Conectada.Controllers
+{
+    //GET
+    public class PersonasController : Controller
+    {
+        public IActionResult Index()
+        {
+            var personas = PersonasRepo.GetAll();
+            return View(personas);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Persona());
+        }
+
+        //POST
+        [HttpPost]
+        public IActionResult Create(Persona model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(model);
+            }
+            PersonasRepo.Agregar(model);
+
+            TempData["Mensaje"] = $"Persona '{model.Nombre}' agregada correctamente.";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
